ff08805c957c718790d0f71a67305984688f3ee1
etsiiproyects <etsiiproyects@gmail.com>
2020-04-25 21:59:46 +0200

    l

--- Proyecto/src/main/java/aiss/SearchController.java
+++ Proyecto/src/main/java/aiss/SearchController.java
@@ -19,49 +19,50 @@ import aiss.model.tmaster.TicketSearch;
 
 public class SearchController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private static final Logger log = Logger.getLogger(SearchController.class.getName());
-	
+
 	public SearchController() {
 		super();
 	}
-	
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		String accessToken=(String) req.getSession().getAttribute("Spotify-token");
-		if(accessToken!=null && !"".equals(accessToken)) {
+		String accessToken = (String) req.getSession().getAttribute("Spotify-token");
+		
+		if (accessToken != null && !"".equals(accessToken)) {
+			String query = req.getParameter("searchQuery");
+			RequestDispatcher rd = null;
+
+			log.log(Level.FINE, "Buscando eventos de " + query);
 			
-		
-		String query = req.getParameter("searchQuery");
-		RequestDispatcher rd = null;
-		
-		// Sample log
-		log.log(Level.FINE, "Buscando eventos de " + query);
-		TMasterResource tmaster = new TMasterResource();
-		SpotifyResource spoty = new SpotifyResource();
-		SpotifyTrackResource spotytracks=new SpotifyTrackResource();
-		TicketSearch tmasterResults =tmaster.getTickets(query);
-		SpotifySearch id = spoty.getArtistsId(query);
-		TracksSearch spotyResults=spotytracks.getArtistTrack(id.getArtists().getItems().get(0).getId());
-		 
-		if(tmasterResults!=null && spotyResults!=null) {
-			rd=req.getRequestDispatcher("/success.jsp");
-			req.setAttribute("tickets", tmasterResults.getEmbedded());
-			req.setAttribute("tracks", spotyResults.getTracks());
+			TMasterResource tmaster = new TMasterResource();
+			SpotifyResource spoty = new SpotifyResource();
+			SpotifyTrackResource spotytracks = new SpotifyTrackResource();
+			
+			TicketSearch tmasterResults = tmaster.getTickets(query);
+			SpotifySearch id = spoty.getArtistsId(query);
+			TracksSearch spotyResults = spotytracks.getArtistTrack(id.getArtists().getItems().get(0).getId());
+
+			if (tmasterResults != null /*&& spotyResults != null*/) {
+				rd = req.getRequestDispatcher("/success.jsp");
+				req.setAttribute("tickets", tmasterResults.getEmbedded());
+				req.setAttribute("tracks", spotyResults.getTracks());
+			} else {
+				log.log(Level.SEVERE, "Objeto TMaster: " + tmasterResults);
+				log.log(Level.SEVERE, "Objeto Spotify: " + spotyResults);
+				rd = req.getRequestDispatcher("/error.jsp");
+			}
+			rd.forward(req, resp);
+
 		} else {
-			log.log(Level.SEVERE, "Objeto TMaster: " + tmasterResults);
-			log.log(Level.SEVERE, "Objeto Spotify: " + spotyResults);
-			rd=req.getRequestDispatcher("/error.jsp");
-		}
-		rd.forward(req, resp);
-		
-		}else {
 			log.info("Intenta acceder a Spotify sin token");
-			req.getRequestDispatcher("AuthController/Spotify").forward(req,resp);
+			req.getRequestDispatcher("AuthController/Spotify").forward(req, resp);
 		}
 	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
-	
+
 }
--- Proyecto/src/main/java/aiss/model/resources/TMasterResource.java
+++ Proyecto/src/main/java/aiss/model/resources/TMasterResource.java
@@ -16,22 +16,11 @@ public class TMasterResource {
 	private static final String TMASTER_API_KEY = "iLoEEaWKhcfBwcfF4u825ExfLQgKkNmB";		// TODO: Change this API KEY for your personal Key
 	private static final Logger log = Logger.getLogger(TMasterResource.class.getName());
 
-	
-//	public TicketSearch getTickets(String ciu, String s, String r) throws UnsupportedEncodingException{
 	public TicketSearch getTickets(String word) throws UnsupportedEncodingException {	
 		
 		String keyword = URLEncoder.encode(word, "UTF-8");
-		
-//		String sal[]=URLEncoder.encode(s, "UTF-8").split("/");
-//		String salida=String.format("%d-%d-%dT00:00:00Z", sal[0], sal[1], sal[2]);
-//		
-//		String reg[]=URLEncoder.encode(r, "UTF-8").split("/");
-//		String regreso=String.format("%d-%d-%dT00:00:00Z", reg[0], reg[1], reg[2]);
-		
-//		String uri="https://app.ticketmaster.com/discovery/v2/events?apikey=" + TMASTER_API_KEY + "&locale=*&startDateTime=" + salida + "&endDateTime=" + regreso +"&city=" + ciudad;
-//		log.log(Level.FINE, "TMaster URI: " + uri);
-		
-		String uri="https://app.ticketmaster.com/discovery/v2/events?apikey=" + TMASTER_API_KEY + "&keyword=" + keyword;
+	
+		String uri="https://app.ticketmaster.com/discovery/v2/events?apikey=" + TMASTER_API_KEY + "&keyword=" + keyword + "&locale=*";
 		log.log(Level.FINE, "TMaster URI: " + uri);
 		
 		ClientResource cr= new ClientResource(uri);
