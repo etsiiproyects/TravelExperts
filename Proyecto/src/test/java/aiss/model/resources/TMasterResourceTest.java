4a8d2bd0284bebb5eba02ffce5d9c0f002baf181
Romero <etsiiproyects@gmail.com>
2020-05-19 17:32:15 +0200

    romero

--- Proyecto/src/test/java/aiss/model/resources/TMasterResourceTest.java
+++ Proyecto/src/test/java/aiss/model/resources/TMasterResourceTest.java
@@ -1,30 +1,22 @@
 package aiss.model.resources;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertNotNull;
 
 import java.io.UnsupportedEncodingException;
 
-
 import org.junit.Test;
-
-
 import aiss.model.tmaster.TicketSearch;
 
 public class TMasterResourceTest {
 	
-	static TicketSearch ticket1, ticket2, ticket3;
 	static TMasterResource tmr = new TMasterResource();
-	static String name = "Aitana";  
-	
 
 	@Test
 	public void testGetTicket() throws UnsupportedEncodingException {
-		TicketSearch ts = tmr.getTickets(name);
+		TicketSearch ts = tmr.getTickets("Aitana");
 		
-		assertEquals("The events of the artist do not match", ticket1.getEmbedded().getEvents(), ts.getEmbedded().getEvents());
+		assertNotNull("No hay eventos para el artista", ts.getEmbedded().getEvents());
 		
-		// Show result
 		System.out.println("Events of the artists: " +  ts.getEmbedded().getEvents());
 		
 	}
