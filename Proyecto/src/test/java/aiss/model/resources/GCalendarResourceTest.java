0dfcbf18ceeee0933cc739fc371d957a79d7dd47
Romero <etsiiproyects@gmail.com>
2020-05-20 14:18:43 +0200

    junit

--- Proyecto/src/test/java/aiss/model/resources/GCalendarResourceTest.java
+++ Proyecto/src/test/java/aiss/model/resources/GCalendarResourceTest.java
@@ -1,5 +1,24 @@
 package aiss.model.resources;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
+
+import aiss.model.gcalendar.GCalendarSearch;
+
 public class GCalendarResourceTest {
+	
+	private static final String access_token = "";
+	
+	static GCalendarResource gcr = new GCalendarResource(access_token);
+	
+	@Test
+	public void testGetEvents() throws UnsupportedEncodingException {
+		GCalendarSearch gcs = gcr.getEvents("etsiiprojects@gmail.com");
+		assertNotNull("No hay eventos para el ID", gcs.getItems());
+		System.out.println("Eventos para el ID: " + gcs.getItems());
+	}
 
 }
--- Proyecto/src/test/java/aiss/model/resources/SpotifyResourceTest.java
+++ Proyecto/src/test/java/aiss/model/resources/SpotifyResourceTest.java
@@ -1,5 +1,36 @@
 package aiss.model.resources;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
+
+import aiss.model.spotifysearch.SpotifySearch;
+import aiss.model.spotifytracks.TracksSearch;
+
 public class SpotifyResourceTest {
+	
+	private final static String access_token = "";
+	
+	static SpotifyResource sr = new SpotifyResource(access_token);
+	static SpotifyTrackResource str = new SpotifyTrackResource(access_token);
+	
+	@Test
+	public void testGetId() throws UnsupportedEncodingException {
+		SpotifySearch ss = sr.getArtistsId("Aitana");
+		assertNotNull("No existe un ID para el artista", ss.getAdditionalProperties().get("Aitana"));
+		System.out.println("ID del artista: " + ss.getAdditionalProperties().get("Aitana"));
+	}
+	
+	@Test
+	public void testGetTracks() throws UnsupportedEncodingException {
+		SpotifySearch ss1 = sr.getArtistsId("Aitana");
+		String id = (String) ss1.getAdditionalProperties().get("Aitana");
+		TracksSearch ss2 = str.getArtistTrack(id);
+		assertNotNull("No existen canciones para el artista", ss2.getTracks());
+		System.out.println("Canciones del artista: " + ss2.getTracks());
+	}
+	
 
 }
