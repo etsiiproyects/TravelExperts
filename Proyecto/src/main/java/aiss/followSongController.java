0ce501813b183f9e3c0e7b75e60a358c0a580cef
etsiiproyects <62990761+etsiiproyects@users.noreply.github.com>
2020-05-24 11:59:09 +0200

    Add files via upload

---
+++ Proyecto/src/main/java/aiss/followSongController.java
@@ -0,0 +1,55 @@
+package aiss;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import aiss.model.resources.SpotifyPlaylistResource;
+
+public class followSongController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public followSongController() {
+        super();
+  
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String accessTokenSpotify = (String) request.getSession().getAttribute("Spotify-token");
+		request.setAttribute("tokenS", accessTokenSpotify);
+		request.setAttribute("gc", "true");
+		
+		if((accessTokenSpotify != null && !"".equals(accessTokenSpotify))) {
+			request.setAttribute("spot", "true");
+			String songId=request.getParameter("songId");
+			SpotifyPlaylistResource spr=new SpotifyPlaylistResource(accessTokenSpotify);
+			boolean success=spr.followSong(songId);
+			
+			if (success) {
+				request.setAttribute("message", "Canción se ha comenzado a seguir");
+			
+			}
+			else {
+				request.setAttribute("message", "La canción no se ha comenzado a seguir");
+			}
+		}else {
+			request.setAttribute("spot", "false");
+		}
+		
+		request.getRequestDispatcher("/SearchControllerOption").forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
